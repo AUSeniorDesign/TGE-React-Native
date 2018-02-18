a9f97cf4ca6984833d1aa5c592cfe8b9c9ace2b5
Harry Summers <hsummers96@gmail.com>
2018-02-18 15:46:08 -0600

    Create user endpoint

--- Components/Login.js
+++ Components/Login.js
@@ -16,17 +16,50 @@ export default class Login extends Component {
 
     async _logInFB(navigate) {
         const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('325985624578151', {
-            permissions: ['public_profile'],
+            permissions: ['public_profile','email'],
           });
         if (type === 'success') {
           // Get the user's name using Facebook's Graph API
           const response = await fetch(
-            `https://graph.facebook.com/me?access_token=${token}`);
-            console.log(token)
-            navigate('Content')
+            `https://graph.facebook.com/me?access_token=${token}`)
 
+            var res = await response.json()
+            // var name = `${(await response.json()).name}`
+            // var id = `${(await response.json()).id}`
+            var email = 'hsummers96@gmail.com'
+            var name = res.name
+            var id = res.id
+
+            console.log(name)
+            console.log(id)
+            console.log(email)
+
+            this._createUser(id, name, token, email)
+            
         }
-      }
+    }
+
+    _createUser(facebookId, name, token, email) {
+
+        
+        //    {"facebook":{"facebookId":"123456789","name":"Haven Barnes","token":"abcdefghijkXYZ_123"}}
+        var body = {
+            "facebook": {
+                "facebookId": facebookId,
+                "name": name,
+                "token": token,
+            }
+        }
+        fetch('http://172.20.10.7:3000/users/create/facebook', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(res => console.log(res.json()))
+            .then(() => console.log('success'))
+            .catch(err => console.log('failure' + err))
+    }
+    
     
     render() {
         const { navigate } = this.props.navigation 
